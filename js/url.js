a4d0bc9f587ef445f132c24e44e42b0243d64eae
Elie <moussaelie497@gmail.com>
2024-08-14 19:38:17 +0300

    fixing again

--- js/url.js
+++ js/url.js
@@ -12,22 +12,8 @@
     if (queryParams.hidePage === 'true') {
       const page4 = document.getElementById('page-4');
       if (page4) {
-        // Remove the item from the DOM
-        page4.parentNode.removeChild(page4);
-        
-        // Reinitialize the carousel if necessary (specific to Bootstrap)
-        const carouselElement = document.querySelector('.carousel');
-        if (carouselElement) {
-          const carousel = bootstrap.Carousel.getInstance(carouselElement);
-          if (carousel) {
-            carousel._items = carouselElement.querySelectorAll('.carousel-item');
-            carousel._config = {...carousel._config, interval: 5000}; // Update as per your configuration
-            carousel.cycle();
-          }
-        }
+        page4.style.display = 'none';
       }
     }
   });
-
-
 
